637e9841a708d4d15cf6b90d06b5bd8b54691a48
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2018-03-05 16:46:20 +0000

    refactor js to get quiz result

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,90 +1,59 @@
-
-function getHighestScore(list, score) {
-  for (i=0; i<list.length; i++) {
-    if (list[i].checked) {
-      if (list[i].value == 'v1' && score<1) {
+function getHighestScore(answersList, score=0) {
+  for (var i=0; i<answersList.length; i++) {
+    if (answersList[i].checked) {
+      if (answersList[i].value == 'v1' && score<1) {
         score = 1
       }
-      else if (list[i].value == 'v2' && score<2) {
+      else if (answersList[i].value == 'v2' && score<2) {
         score = 2
       }
-      else if (list[i].value == 'v3' && score<3) {
+      else if (answersList[i].value == 'v3' && score<3) {
         score = 3
       }
-      else if (list[i].value == 'v4' && score<4) {
+      else if (answersList[i].value == 'v4' && score<4) {
         score = 4
       }
-    }
+    } 
   }
-  // console.log('highest score is')
-  // console.log(score)
   return score
 }
 
-function tabulateAnswers() {
-  // initialize variables for each score
-  var depressionScore = 0;
-  var angerScore = 0;
-  var maniaScore = 0;
-  var anxietyScore = 0;
-  var somaticScore = 0;
-  var suicidalIdeationScore = 0;
-  var psychosisScore = 0;
-  var sleepProbScore = 0;
-  var memoryScore = 0;
-  var repetitiveTnBscore = 0;
-  var dissociationScore = 0;
-  var persoFunctioningScore = 0;
-  var substUseScore = 0;
+function tabulateAnswers(conditionDomains) {
+  var scores = []
+  for (var i=0; i<conditionDomains.length; i++) {
+    var condition = conditionDomains[i];
+    // // get a list of the radio inputs per symptoms domain
+    var conditionQs = document.getElementById(condition).getElementsByTagName('input');
+    // // then find score for each symptoms domain
+    var conditionScore = getHighestScore(conditionQs);
+    scores[condition] = conditionScore;
+  }
+  return scores
+}
 
-  // get a list of the radio inputs per symptoms domain
-  var domain1 = document.getElementById('depression').getElementsByTagName('input');
-  // then find score
-  var domain1score = getHighestScore(domain1, depressionScore);
-  // console.log('domain1score is')
-  // console.log(domain1score)
-
-  var domain2 = document.getElementById('anger').getElementsByTagName('input');
-  var domain2score = getHighestScore(domain2, angerScore);
-
-  var domain3 = document.getElementById('mania').getElementsByTagName('input');
-  var domain3score = getHighestScore(domain3, maniaScore);
-
-  var domain4 = document.getElementById('anxiety').getElementsByTagName('input');
-  var domain4score = getHighestScore(domain4, anxietyScore);
-
-  var domain5 = document.getElementById('somatic').getElementsByTagName('input');
-  var domain5score = getHighestScore(domain5, somaticScore);
-
-  var domain8 = document.getElementById('sleep').getElementsByTagName('input');
-  var domain8score = getHighestScore(domain8, sleepProbScore);
-
-  var domain9 = document.getElementById('memory').getElementsByTagName('input');
-  var domain9score = getHighestScore(domain9, memoryScore);
-
-  var domain10 = document.getElementById('repetitiveTnB').getElementsByTagName('input');
-  var domain10score = getHighestScore(domain10, repetitiveTnBscore);
-
-  var domain11 = document.getElementById('dissociation').getElementsByTagName('input');
-  var domain11score = getHighestScore(domain11, dissociationScore);
-
-  var domain12 = document.getElementById('persoFunction').getElementsByTagName('input');
-  var domain12score = getHighestScore(domain12, persoFunctioningScore);
-
-  // listing 3 most serious conditions below; these will need to display 
-  // follow-up assessment from score of 1 (2 and above for other conditions)
-  var domain6 = document.getElementById('suicidalIdeation').getElementsByTagName('input');
-  var domain6score = getHighestScore(domain6, suicidalIdeationScore);
-
-  var domain7 = document.getElementById('psychosis').getElementsByTagName('input');
-  var domain7score = getHighestScore(domain7, psychosisScore);
-
-  var domain13 = document.getElementById('substanceUse').getElementsByTagName('input');
-  var domain13score = getHighestScore(domain13, substUseScore);
+function quizResult() {
+  var conditionsList = [
+    "depression",
+    "anger",
+    "mania",
+    "anxiety",
+    "somatic",
+    "sleep",
+    "memory",
+    "repetitiveTnB",
+    "dissociation",
+    "persoFunction",
+    "suicidalIdeation",
+    "psychosis",
+    "substanceUse"
+  ]
+  var result = tabulateAnswers(conditionsList);
+  // console.log(result);
+  return result
 }
 
 // // Display result
-// var answerbox = document.getElementById('answer');
+// function resultbox(condition, score) {
 //   if (domain1score >1) {
 //     // answerbox.innerHTML =
 //   }
@@ -93,4 +62,5 @@ function tabulateAnswers() {
 //   }
 // }
 
-document.getElementById("submit").addEventListener("click", tabulateAnswers)
+document.getElementById("submit").addEventListener("click", quizResult)
+// document.getElementById("result").innerHTML = resultbox
